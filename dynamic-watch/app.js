697541d8080f2d3b3b7b6b5298e155a0d1d6910a
ryuzaki979 <67687048+ryuzaki979@users.noreply.github.com>
2020-11-27 09:14:42 +0900

    added the year section

--- dynamic-watch/app.js
+++ dynamic-watch/app.js
@@ -2,6 +2,9 @@ let  hour = document.getElementById('hour');
 let  minute = document.getElementById('minute');
 let  second = document.getElementById('second');
 let  progress = document.getElementById('progress');
+let  showDay = document.getElementById('showDay');
+let  showMonth = document.getElementById('showMonth');
+let  showYear = document.getElementById('showYear');
 
 function getTime(){
     let time = new Date;
@@ -12,6 +15,12 @@ function getTime(){
     let sec = time.getSeconds();
     second.innerText= sec;
     progress.style.width = (sec/60) * 100 + '%';
+    let day= time.getDate();
+    showDay.innerText = day;
+    let month = time.getMonth();
+    showMonth.innerText= month;
+    let year = time.getFullYear();
+    showYear.innerText = year;
 }
 
 setInterval(getTime,1000);
